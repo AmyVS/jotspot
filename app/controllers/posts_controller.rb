f49e90b93113a5fd5bd63941f435005b456e3239
amyvs <amydecat@yahoo.com>
2014-09-21 20:08:59 -0700

    add content to README.md

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,10 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    respond_to do |format|
+      format.html { redirect_to user_path(current_user) }
+      format.js
+    end
   end
 
   def create
---
+++ spec/features/post_flow_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "creating a post" do
+
+  # it "allows the logged in user to create a new post" do
+  #   user = FactoryGirl.create(:user)
+  #   login(user)
+  #   visit user_path(user)
+  #   click_on "New Post"
+  #   save_and_open_page
+  #   fill_in :post_title, with: "My First Post"
+  #   fill_in :post_content, with: "Here is my first post. Isn't it great?!"
+  #   click_on "Create Post"
+  #   expect(find('#post_list')).to have_content "Your new post has been added."
+  # end
+
+end
