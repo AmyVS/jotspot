2bc5d26adf2e6ff25e014af847575d9bd8fcdd8e
amyvs <amydecat@yahoo.com>
2014-09-21 18:23:48 -0700

    add remove functionality to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,7 +44,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to user_path(current_user)
   end
 
 private
